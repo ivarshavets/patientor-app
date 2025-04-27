6aa6cc3bbe63593ae948fd98a37704300cdeda79
Ira Varshavets <varshavets@gmail.com>
2025-04-27 21:01:58 +0200

    Added Data with patients data. Added models for future DB. Added routes for patients

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 
+import patientsRouter from './routes/patients'
+
 const app = express();
 app.use(cors())
 
 app.use(express.json());
 
 const PORT = 3001;
+
+app.use('/api/patients', patientsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ patientor-backend/src/models/patient.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const patientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  dateOfBirth: {
+    type: String,
+    required: true
+  },
+  ssn: {
+    type: String,
+  },
+  gender: String,
+  occupation: String,
+  diagnose: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Diagnose'
+  }
+})
+
+// Even though the _id property of Mongoose objects looks like a string, it is in fact an object. The toJSON method we defined transforms it into a string just to be safe.
+patientSchema.set('toJSON', {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Patient', patientSchema)
